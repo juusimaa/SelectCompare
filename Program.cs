cba5f71d19329945e85c74b961a45f51d30b5c6c
Jouni Uusimaa <jouni.uusimaa@gmail.com>
2012-06-29 10:38:34 +0300

    Work in progress

--- Program.cs
+++ Program.cs
@@ -15,8 +15,14 @@ namespace SelectCompare
         static int Main(string[] args)
         {
             List<Util> m_utils = new List<Util>();
-            parseSettings("settings.xml", ref m_utils);
-            Util selectedUtility = findUtility(ref m_utils, args[0]);
+            parseSettings(AppDomain.CurrentDomain.BaseDirectory + "\\settings.xml", ref m_utils);
+            if (m_utils == null)
+            {
+                System.Console.WriteLine("ERROR: Parsing 'settings.xml' failed.");
+                return -1;
+            }
+
+            Util selectedUtility = findUtility(ref m_utils, args);
             if (selectedUtility == null)
             {
                 System.Console.WriteLine("ERROR: No utility registered for this file type.");
@@ -24,6 +30,8 @@ namespace SelectCompare
             }
 
             string arguments;
+
+            // if only two arguments, assume diff
             if (args.Length == 2)
                 arguments = getDiffCommand(selectedUtility, args);
             else
@@ -31,15 +39,16 @@ namespace SelectCompare
 
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
             startInfo.FileName = selectedUtility.Exe;
             startInfo.Arguments = arguments;
 
             try
             {
                 System.Console.WriteLine("Using utility " + selectedUtility.Name);
+                System.Console.WriteLine("Command: " + startInfo.FileName + startInfo.Arguments);
                 process.StartInfo = startInfo;
                 process.Start();
+                process.WaitForExit();
             }
             catch (ArgumentNullException)
             {
@@ -61,19 +70,25 @@ namespace SelectCompare
         /// <param name="p_utils"></param>
         /// <param name="p_extension"></param>
         /// <returns></returns>
-        private static Util findUtility(ref List<Util> p_utils, string filename)
+        private static Util findUtility(ref List<Util> p_utils, string[] p_args)
         {
-            string ext = Path.GetExtension(filename);
-            foreach (Util utility in p_utils)
+            foreach (string argument in p_args)
             {
-                if (utility.Extensions.Contains(ext))
-                    return utility;
+                string currentExt = Path.GetExtension(argument);
+                foreach (Util utility in p_utils)
+                {
+                    foreach (string ext in utility.Extensions)
+                    {
+                        if (currentExt.CompareTo(ext) == 0)
+                            return utility;
+                    }
+                }
             }
             return null;
         }
 
         /// <summary>
-        /// 
+        /// Operation to get diff command line options for selected utility.
         /// </summary>
         /// <param name="p_selectedUtil"></param>
         /// <param name="p_args"></param>
@@ -91,18 +106,43 @@ namespace SelectCompare
         /// <returns></returns>
         private static string getMergeCommand(Util p_selectedUtil, string[] p_args)
         {
-            return string.Empty;
+            int index = 0;
+            string command = string.Empty;
+
+            foreach (string str in p_args)
+            {
+                if (str.CompareTo(p_selectedUtil.BaseSwitch) == 0)
+                    command += " " + str + " " + p_args[index + 1];
+                else if (str.CompareTo(p_selectedUtil.OutputSwitch) == 0)
+                    command += " " + str + " " + p_args[index + 1];
+                else if (str.CompareTo(p_selectedUtil.MergeSwitch) == 0)
+                    command += " " + str + " " + p_args[index + 1];
+                else if (str.Contains(p_selectedUtil.ExtraOptions))
+                    command += " " + str;
+                
+                index++;
+            }
+
+            return command;
         }
 
         /// <summary>
-        /// 
+        /// Operation to parse 'settings.xml' file.
         /// </summary>
         /// <param name="p_filename"></param>
         /// <param name="p_utils"></param>
         private static void parseSettings(string p_filename, ref List<Util> p_utils)
         {
             XmlDocument settings = new XmlDocument();
-            settings.Load(p_filename);
+
+            try
+            {
+                settings.Load(p_filename);
+            }
+            catch (XmlException)
+            {
+                p_utils = null;
+            }
 
             foreach (XmlNode node1 in settings)
             {
@@ -118,6 +158,14 @@ namespace SelectCompare
                                 tmp.Exe = node4.InnerText;
                             if (node4.Name.CompareTo("ext") == 0)
                                 tmp.Extensions.Add(node4.InnerText);
+                            if (node4.Name.CompareTo("merge") == 0)
+                                tmp.MergeSwitch = node4.InnerText;
+                            if (node4.Name.CompareTo("base") == 0)
+                                tmp.BaseSwitch = node4.InnerText;
+                            if (node4.Name.CompareTo("output") == 0)
+                                tmp.OutputSwitch = node4.InnerText;
+                            if (node4.Name.CompareTo("extraoptions") == 0)
+                                tmp.ExtraOptions = node4.InnerText;
                         }
                         p_utils.Add(tmp);
                     }
@@ -127,35 +175,85 @@ namespace SelectCompare
     }
 
     /// <summary>
-    /// 
+    /// Class for holding compare/merge utility parameters.
     /// </summary>
     class Util
     {
+        private string m_name;
+        private string m_exe;
+        private string m_extra;
+        private string m_baseSwitch;
+        private string m_outputSwitch;
+        private List<string> m_extensions;
+
         public Util()
         {
             m_extensions = new List<string>();
         }
 
-        private string m_name;
+        private string m_mergeSwitch;
+
+        /// <summary>
+        /// Swtich for merge input file.
+        /// </summary>
+        public string MergeSwitch
+        {
+            get { return m_mergeSwitch; }
+            set { m_mergeSwitch = value; }
+        }        
+
+        /// <summary>
+        /// Swtich for base file.
+        /// </summary>
+        public string BaseSwitch
+        {
+            get { return m_baseSwitch; }
+            set { m_baseSwitch = value; }
+        }       
+
+        /// <summary>
+        /// Switch for output file.
+        /// </summary>
+        public string OutputSwitch
+        {
+            get { return m_outputSwitch; }
+            set { m_outputSwitch = value; }
+        }        
+
+        /// <summary>
+        /// Extra command line switches for utility.
+        /// </summary>
+        public string ExtraOptions
+        {
+            get { return m_extra; }
+            set { m_extra = value; }
+        }
+	
+        /// <summary>
+        /// Utility name.
+        /// </summary>
         public string Name
         {
             get { return m_name; }
             set { m_name = value; }
         }
 
-        private string m_exe;
+        /// <summary>
+        /// Full path to executable.
+        /// </summary>
         public string Exe
         {
             get { return m_exe; }
             set { m_exe = value; }
         }
 
-        private List<string> m_extensions;
+        /// <summary>
+        /// Extension supported.
+        /// </summary>
         public List<string> Extensions
         {
             get { return m_extensions; }
             set { m_extensions = value; }
         }
-
     }
 }
